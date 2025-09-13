ba5b6cf23ad6e9be56e491b178b1083a139047da
dtgamor <diego.tgamor@gmail.com>
2025-09-13 12:50:49 -0300

    initial commit for task2

--- app.js
+++ app.js
@@ -1,96 +1,49 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 
-
-//SOMA
-app.get('/soma/:numUm/:numDois', (req, res) => {
+app.get('/calculadora/', (req, res) => {
     try {
-        const { numUm, numDois } = req.params;
 
+        const { operacao, numUm, numDois } = req.query;
+        
+        if (
+            isNaN(numUm) || isNaN(numDois) || operacao.includes(["soma", "subtracao", "multiplicacao", "divisao"])
+        ) {
+            return res.status(405).send("É obrigatório informar uma operação válida e dois números válidos");
+        }
+        
         const numero1 = Number(numUm);
         const numero2 = Number(numDois);
+        let resultado;
 
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return res.status(400).send(`É obrigatório informar dois números válidos.`);
+        switch (operacao) {
+            case "soma":
+                resultado = numero1 + numero2;
+                break;
+            case "subtracao":
+                if (numero2 < 0) {
+                    return res.status(405).send("Não é possivel subtrair de um valor negativo")
+                }
+                resultado = numero1 - numero2;
+                break;
+            case "multiplicacao":
+                resultado = numero1 * numero2;
+                break;
+            case "divisao":
+                if (numero2 === 0) {
+                    return res.status(405).send("Não é possível dividir por zero");
+                }
+                resultado = numero1 / numero2;
+                break;
         }
 
-        const resultado = numero1 + numero2;
-
-        res.status(200).send(`Resultado da soma é: ${resultado}`);
-
+        res.status(200).send(`Resultado da operação é: ${resultado}`);
     } catch (error) {
         console.error("Erro ao executar a operação:", error);
-        res.status(500).send(`Erro interno`);
+        res.status(500).send("Erro interno");
     }
 });
-
-//SUBTRAÇÃO
-app.get('/subtracao/:numUm/:numDois', (req, res) => {
-    try {
-        const { numUm, numDois } = req.params;
-
-        const numero1 = Number(numUm);
-        const numero2 = Number(numDois);
-
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return res.status(400).send(`É obrigatório informar dois números válidos.`);
-        }
-
-        const resultado = numero1 - numero2;
-
-        res.status(200).send(`Resultado da subtração é: ${resultado}`);
-
-    } catch (error) {
-        console.error("Erro ao executar a operação:", error);
-        res.status(500).send(`Erro interno`);
-    }
-});
-
-//MULTIPLICAÇÃO
-app.get('/multiplicacao/:numUm/:numDois', (req, res) => {
-    try {
-        const { numUm, numDois } = req.params;
-
-        const numero1 = Number(numUm);
-        const numero2 = Number(numDois);
-
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return res.status(400).send(`É obrigatório informar dois números válidos.`);
-        }
-
-        const resultado = numero1 * numero2;
-
-        res.status(200).send(`Resultado da multiplicação é: ${resultado}`);
-
-    } catch (error) {
-        console.error("Erro ao executar a operação:", error);
-        res.status(500).send(`Erro interno`);
-    }
-});
-
-//DIVISÃO
-app.get('/divisao/:numUm/:numDois', (req, res) => {
-    try {
-        const { numUm, numDois } = req.params;
-
-        const numero1 = Number(numUm);
-        const numero2 = Number(numDois);
-
-        if (isNaN(numero1) || isNaN(numero2) || (numero1) != 0 || (numero2) !=0) {
-            return res.status(400).send(`É obrigatório informar dois números válidos e maiores que 0.`);
-        }
-
-        const resultado = numero1 / numero2;
-
-        res.status(200).send(`Resultado da divisão é: ${resultado}`);
-
-    } catch (error) {
-        console.error("Erro ao executar a operação:", error);
-        res.status(500).send(`Erro interno`);
-    }
-});
-
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${PORT}`);
