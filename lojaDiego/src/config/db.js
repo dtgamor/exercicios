b159fdbfc61ed8751e0c7e8926c4f183b7e2b014
dtgamor <diego.tgamor@gmail.com>
2025-10-04 15:04:44 -0300

    part1

---
+++ lojaDiego/src/config/db.js
@@ -0,0 +1,22 @@
+const sql = require("mssql");
+const CONFIG = {
+    user: 'sa',
+    password: '123456789',
+    server: 'localhost',
+    database: 'LojaDB',
+    options:{
+        encrypt: true,
+        trustServerCertificate: true
+    }
+}
+
+async function getConnection() {
+    try {
+        const pool = await sql.connect(CONFIG);
+        return pool;  
+    } catch (error){
+        console.error('Erro na conexão SQL Server:',error);
+
+    }
+    
+}
